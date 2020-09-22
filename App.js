9554b21e92bbb2f475b0621c6cc5bbfd5f9642be
hoangthang <inabasalvere0409@gmail.com>
2020-09-22 09:22:12 +0000

    fix

--- App.js
+++ App.js
@@ -1,42 +1,80 @@
 import React from 'react';
-import { SafeAreaView, View, FlatList, StyleSheet, Text, StatusBar, Image } from 'react-native';
+import { SafeAreaView, View, FlatList, StyleSheet, Text, StatusBar, Image, TouchableOpacity } from 'react-native';
 
 const DATA = [
   {
-    id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba',
+    id: '1',
     title: 'First Item',
     text: 'test',
     image: require('./img/1.jpg')
   },
   {
-    id: '3ac68afc-c605-48d3-a4f8-fbd91aa97f63',
+    id: '2',
     title: 'Second Item',
     text: 'test',
     image: require('./img/1.jpg')
   },
   {
-    id: '58694a0f-3da1-471f-bd96-145571e29d72',
+    id: '3',
+    title: 'Third Item',
+    text: 'test',
+    image: require('./img/1.jpg')
+  },
+  {
+    id: '4',
+    title: 'Third Item',
+    text: 'test',
+    image: require('./img/1.jpg')
+  },
+  {
+    id: '5',
+    title: 'Third Item',
+    text: 'test',
+    image: require('./img/1.jpg')
+  },
+  {
+    id: '6',
+    title: 'Third Item',
+    text: 'test',
+    image: require('./img/1.jpg')
+  },
+  {
+    id: '7',
+    title: 'Third Item',
+    text: 'test',
+    image: require('./img/1.jpg')
+  },
+  {
+    id: '8',
     title: 'Third Item',
     text: 'test',
     image: require('./img/1.jpg')
   },
 ];
 
-const Item = ({ title, text, image}) => (
+const Item = ({ title, text, image }) => (
 
   <View style={styles.item}>
     <Image source={image} style={styles.image}></Image>
-    <Text style={styles.title}>{text}</Text>
-    <Text style={styles.title}>{title}</Text>
+    <View style={{ alignItems: "left", flex: 1 }}>
+      <Text style={styles.title}>{text}</Text>
+      <Text style={styles.title}>{title}</Text>
+    </View>
+    <TouchableOpacity>
+      <TouchableOpacity style={{ height: 50, width: 70, justifyContent: "center", alignItems: "left" }}>
+        <Text style={{ color: "green" }}>AddToCart</Text>
+      </TouchableOpacity>
+    </TouchableOpacity>
+
   </View>
 );
 
 const App = () => {
   const renderItem = ({ item }) => (
-    <Item title={item.title} 
-          text={item.text}
-          image={item.image} 
-          />
+    <Item title={item.title}
+      text={item.text}
+      image={item.image}
+    />
   );
 
   return (
@@ -53,16 +91,21 @@ const App = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    marginTop: StatusBar.currentHeight || 0,
+    backgroundColor: '#F7F7F7',
+    marginTop: 60
   },
   item: {
-    backgroundColor: '#FFFFFF',
+    margin: 10,
     padding: 10,
-    marginVertical: 5,
-    marginHorizontal: 5,
+    backgroundColor: "#FFF",
+    width: "80%",
+    flex: 1,
+    alignSelf: "center",
+    flexDirection: "row",
+    borderRadius: 5
   },
   title: {
-    fontSize: 32,
+    fontSize: 20,
   },
   image: {
     width: 100,
